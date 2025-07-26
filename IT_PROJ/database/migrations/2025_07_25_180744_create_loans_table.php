45bd9abf4dc75163d244b3b66477a06bb70e46ae
EmihleG <emihlegontsana@gmail.com>
2025-07-26 22:41:32 +0200

    created foreign keys and added loan & due dates

--- IT_PROJ/database/migrations/2025_07_25_180744_create_loans_table.php
+++ IT_PROJ/database/migrations/2025_07_25_180744_create_loans_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('loans', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->date('loan_date');
+            $table->date('due_date');
             $table->timestamps();
         });
     }
