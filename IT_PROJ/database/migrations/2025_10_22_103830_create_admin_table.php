be3bfd6ad1a0b0c35039054bb10650766ea88523
EmihleG <emihlegontsana@gmail.com>
2025-10-22 22:56:13 +0200

    updated admin table

--- IT_PROJ/database/migrations/2025_10_22_103830_create_admin_table.php
+++ IT_PROJ/database/migrations/2025_10_22_103830_create_admin_table.php
@@ -11,8 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('admin', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('mobile')->nullable();
+            $table->string('position')->nullable();
+            $table->string('department')->nullable();
+            $table->string('office')->nullable();
+            $table->string('address')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
